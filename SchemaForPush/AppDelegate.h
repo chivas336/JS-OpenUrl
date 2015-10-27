a00f342b102d4b865b3f2bd5d578ed4c42c7ac75
axiBug7 <1255788419@qq.com>
2015-10-27 10:52:46 +0800

    commit

---
+++ SchemaForPush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SchemaForPush
+//
+//  Created by axiBug on 15/10/27.
+//  Copyright © 2015年 杭州贝宇网络有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
